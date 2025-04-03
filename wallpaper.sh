4bff6410d3881a26149bb345a81945101b262707
Zap F. Dingbats <pazifi@xig.ch>
2025-04-03 09:48:57 +0200

    little bit cleanup

--- wallpaper.sh
+++ wallpaper.sh
@@ -5,12 +5,13 @@
 
 # 2019.??.?? - Created
 # 2020.07.19 - Waiting for Inet connection and logging
+# 2025.04.03 - Little bit cleanup
 
 #######################################################################
 # Configuration
 #######################################################################
 
-# Valid options: "_1024x768" "_1280x720" "_1366x768" "_1920x1200"
+# Valid options: "_1024x768" "_1280x720" "_1366x768" "_1920x1200" 
 RES="_1920x1200"
 ARCH_DIR=${HOME}/bin/wallpaper/archive/
 TIMEOUT=120
@@ -19,6 +20,7 @@ TIMEOUT=120
 # Functions
 #######################################################################
 function log {
+	echo ${1}
 	logger -t "$(basename $0)[$$]" ${1};
 }
 
@@ -29,7 +31,7 @@ function log {
 log "Waiting for internet connection"
 while ! ping -4 -c 1 -n -w 1 www.bing.com &> /dev/null; do
 	(( TIMEOUT-- ))
-	if [ $TIMEOUT -lt 0 ]; then
+	if [ ${TIMEOUT} -lt 0 ]; then
 		log "Timeout"
 		exit 0
  	fi
@@ -43,19 +45,17 @@ if ! URL=$(curl -s "http://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=1
 	exit 1
 fi
 
+# destination path for picture
+PICTURE="${URL#/th?id=OHR.}${RES}.jpg"
+
 # download picture
-if ! wget http://www.bing.com/$URL$RES.jpg -P $ARCH_DIR; then
+if ! wget http://www.bing.com/${URL}${RES}.jpg -O ${ARCH_DIR}${PICTURE}; then
 	log "Can not download picture"
 	exit 1;
 fi
 
-# rename special characters in path (dirty)
-for i in $(ls -1hrt $ARCH_DIR  ); do if [[ $i  == *"th?id=OHR."* ]]; then mv $ARCH_DIR/$i  $ARCH_DIR/${i#"th?id=OHR."} ; fi   ; done
-
-# path of the latest picture
-PIC=$ARCH_DIR/$(ls -1hrt $ARCH_DIR | tail -n1)
-
-log "install picture \"${PIC}\" as wallpaper on all desktops"
+# Install picture as wallpaper in KDE
+log "install picture \"${PICTURE}\" as wallpaper on all desktops"
 dbus-send --session --dest=org.kde.plasmashell --type=method_call /PlasmaShell org.kde.PlasmaShell.evaluateScript "string:
 var Desktops = desktops();                                                                                                                       
 for (i=0;i<Desktops.length;i++) {
@@ -64,5 +64,5 @@ for (i=0;i<Desktops.length;i++) {
         d.currentConfigGroup = Array(\"Wallpaper\",
                                     \"org.kde.image\",
                                     \"General\");
-        d.writeConfig(\"Image\", \"file://${PIC}\");
+        d.writeConfig(\"Image\", \"file://${PICTURE}\");
 }"
